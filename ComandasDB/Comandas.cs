43d1a3a374ad0d7b9fda70174cac1c3dae0ea23a
Henrique Reis <henriquereis198@outlook.com>
2023-10-25 13:57:32 -0300

    Implementação de contexto do banco, tabelas PreVendas e ItensPreVendas mapeadas e criação de método de teste

---
+++ ComandasDB/Comandas.cs
@@ -0,0 +1,35 @@
+﻿using DatabaseScaffold;
+using System;
+using System.Linq;
+
+
+namespace ComandasDB
+{
+    public class Comandas
+    {
+        internal DataMRPDV _dbConnection { get; set; }
+
+        public Comandas()
+        {
+            _dbConnection = new DataMRPDV();
+        }
+
+        /// <summary>
+        /// Testes <para/>
+        /// Retorna no console uma lista com algumas informações das pré vendas a fim de testes.
+        /// </summary>
+        public static void GetPreVendasInConsole()
+        {
+            using (var db = new DataMRPDV())
+            {
+                var query = from preVendas in db.PreVendas
+                            select preVendas;
+
+                foreach (var preVenda in query)
+                {
+                    Console.WriteLine(preVenda);
+                }
+            }
+        }
+    }
+}
---
+++ ComandasDB/Tables/ItensPreVendas.cs
@@ -0,0 +1,72 @@
+namespace DatabaseScaffold
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class ItensPreVendas
+    {
+        [Key]
+        public int ID_IPRV { get; set; }
+
+        public int NUMERO_PRVD { get; set; }
+
+        public int? ITEM_IPRV { get; set; }
+
+        public int? ID_PRVL { get; set; }
+
+        public decimal? QTDE_IPRV { get; set; }
+
+        public decimal? PRECO_IPRV { get; set; }
+
+        public int? ID_PROM { get; set; }
+
+        public int? QTDEPROM_IPRV { get; set; }
+
+        public int? ITEMPROM_IPRV { get; set; }
+
+        public decimal? DESCPROM_IPRV { get; set; }
+
+        public decimal? DESCONTO_IPRV { get; set; }
+
+        [StringLength(1)]
+        public string UNICAPR_IPRV { get; set; }
+
+        [StringLength(80)]
+        public string OBS_IPRV { get; set; }
+
+        public DateTime? DATAHORA_IPRV { get; set; }
+
+        public int? ID_OPER_AUT { get; set; }
+
+        public int? COD_VEND { get; set; }
+
+        [StringLength(1)]
+        public string SITUACAO_IPRV { get; set; }
+
+        [StringLength(1)]
+        public string IMPRESSAO_IPRV { get; set; }
+
+        public int? COD_PROD { get; set; }
+
+        public int? ID_VLPR01 { get; set; }
+
+        public int? ID_VLPR02 { get; set; }
+
+        public int? ID_VLPR03 { get; set; }
+
+        public int? ID_ITPV_NUVEM { get; set; }
+
+        public DateTime? DATACOM_IPRV { get; set; }
+
+        [StringLength(80)]
+        public string DESCRICAO_IPRV { get; set; }
+
+        public decimal? ACRESCIMO_IPRV { get; set; }
+
+        [StringLength(1)]
+        public string ADICIONAL_IPRV { get; set; }
+    }
+}
