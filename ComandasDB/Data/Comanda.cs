0805cd32cf700b0ecc6df18ab99c1af09325c8c1
Henrique Reis <henriquereis198@outlook.com>
2023-11-01 14:18:11 -0300

    Sem alterações.

---
+++ ComandasDB/Data/Comanda.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ComandasDB.Data
+{
+    public class Comanda 
+    {
+        public PreVendas PreVenda { get; set; }
+        public List<ItensPreVendas> ItensPreVenda { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sBuilder = new StringBuilder();
+
+            sBuilder.AppendLine($"Pré Venda: {PreVenda.NUMERO_PRVD}");
+            sBuilder.AppendLine($"Comanda: {PreVenda.COMANDA_PRVD}");
+            sBuilder.AppendLine($"Cliente: {PreVenda.NOME_PRVD}");
+            sBuilder.AppendLine($"Documento: {PreVenda.CPFCNPJ_PRVD}");
+            sBuilder.AppendLine($"Valor: R${PreVenda.VALOR_PRVD}");
+            sBuilder.AppendLine($"Produtos registrados:");
+            foreach (var item in ItensPreVenda)
+            {
+                sBuilder.AppendLine($"\tProduto: {item.COD_PROD}");
+                sBuilder.AppendLine($"\tValor un: R${item.PRECO_IPRV}");
+                sBuilder.AppendLine($"\tQuantidade: {item.QTDE_IPRV}");
+                sBuilder.AppendLine();
+
+            }
+            sBuilder.AppendLine("=======================================================================================");
+
+            return sBuilder.ToString();
+        }
+    }
+}
--- ComandasDB/Data/Internal/RetrieveFromMRPDV.cs
+++ ComandasDB/Data/Internal/RetrieveFromMRPDV.cs
@@ -1,16 +1,57 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ComandasDB.Data.Internal
 {
-    internal class RetrieveFromMRPDV
+    public class RetrieveFromMRPDV
     {
-        public void RetrievePreVendas()
+        internal static List<PreVendas> GetPreVendasFromMRPDV()
         {
+            List<PreVendas> preVendasList = new List<PreVendas>();
+
             using (var db = new DataMRPDVContext())
             {
-                var query = from preVendas in db.PreVendas
-                            select preVendas;
+                var query = db.PreVendas.Select(s => s);
 
+                foreach (var item in query)
+                {
+                    preVendasList.Add(item);
+                }
+            }
+
+            return preVendasList;
+        }
+
+        internal static List<ItensPreVendas> GetItensPreVendasFromMRPDV()
+        {
+            List<ItensPreVendas> itensPreVendasList = new List<ItensPreVendas>();
+
+            using (var db = new DataMRPDVContext())
+            {
+                var query = db.ItensPreVendas.Select(s => s);
+
+                foreach (var item in query)
+                {
+                    itensPreVendasList.Add(item);
+                }
+            }
+
+            return itensPreVendasList;
+        }
+
+        public static void RetriveCurrentDataFromMrToComandasDatabase()
+        {
+            var preVendasList = GetPreVendasFromMRPDV();
+            var itensPreVendasList = GetItensPreVendasFromMRPDV();
+
+            if (preVendasList.Count() > 0 && itensPreVendasList.Count() > 0)
+            {
+                foreach (var preVenda in preVendasList)
+                {
+                    Comandas.InsertPreVendaOnComandasDb(preVenda);
+                }
+
+                Comandas.InsertItensPreVendaOnComandasDb(itensPreVendasList);
             }
         }
     }
