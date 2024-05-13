2c435ed30083d5ce86e6d36825170f358d73dafa
Henrique Reis <henriquereis198@outlook.com>
2024-05-13 14:42:30 -0300

    Classe que retorna a instância correta para onde será feita a requisição dos dados

---
+++ ComandasDB/ComandaInstance.cs
@@ -0,0 +1,23 @@
+﻿using ComandasDB.Data.Internal;
+
+namespace ComandasDB
+{
+    // TODO: Documentar
+    public class ComandaInstance
+    {
+        public static IComandas GetInstance()
+        {
+            var serverInfo = RetrieveFromMRPDV.GetServerInfo();
+            var PdvInfo = RetrieveFromMRPDV.PdvInfo();
+
+            if (serverInfo == null || int.Parse(serverInfo.VALOR_PAR) == PdvInfo.NUMERO_PDV)
+            {
+                return new ComandasLocal();
+            }
+            else
+            {
+                return new ComandasServer(serverInfo.TEXTO_PAR);
+            }
+        }
+    }
+}
